3ac65e4f41f41f735b3e6453ca9ae8e1bce28bba
Grzegorz Nęcka <strzypior@gmail.com>
2021-07-03 20:50:31 +0200

    write a blog site

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from 'components/Layout';
+import Head from 'next/head';
+export default function About() {
+  return (
+    <Layout className="p-10 ">
+      <Head>
+        <title>about us</title>
+      </Head>
+
+      <p>sss</p>
+    </Layout>
+  );
+}
